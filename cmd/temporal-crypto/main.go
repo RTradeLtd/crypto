8edc6498497e00820042faade7498bd20b688109
Robert Lin <robertlin1@gmail.com>
2018-10-04 19:04:52 -0700

    Initial migration from RTradeLtd/Temporal

---
+++ cmd/temporal-crypto/main.go
@@ -0,0 +1,57 @@
+package main
+
+import (
+	"io/ioutil"
+	"log"
+	"os"
+	"path/filepath"
+
+	"github.com/RTradeLtd/cmd"
+	"github.com/RTradeLtd/config"
+	"github.com/RTradeLtd/crypto"
+)
+
+var commands = map[string]cmd.Cmd{
+	"decrypt": cmd.Cmd{
+		Blurb:       "decrypt file encrypted by Temporal",
+		Description: "Decrypts given files using passphrase set in TEMPORAL_PASSPHRASE. Decrypted files are saved in ./<filename>.decrypted",
+		Action: func(cfg config.TemporalConfig, args map[string]string) {
+			p := os.Getenv("TEMPORAL_PASSPHRASE")
+			if p == "" {
+				log.Fatal("no passphrase provided in TEMPORAL_PASSPHRASE")
+			}
+
+			decrypt := crypto.NewEncryptManager(p)
+			for i := 2; i < len(os.Args); i++ {
+				f, err := os.Open(os.Args[i])
+				if err != nil {
+					log.Fatal(err)
+				}
+				out, err := decrypt.Decrypt(f)
+				if err != nil {
+					log.Fatal(err)
+				}
+
+				dir, err := os.Getwd()
+				if err != nil {
+					log.Fatal(err)
+				}
+
+				if err = ioutil.WriteFile(
+					filepath.Join(dir, filepath.Base(os.Args[i])),
+					out, 0644); err != nil {
+					log.Fatal(err)
+				}
+			}
+		},
+	},
+}
+
+func main() {
+	app := cmd.New(commands, cmd.Config{
+		Name:     "Temporal Encryption Utility",
+		ExecName: "temporal-crypto",
+		Desc:     "Temporal's object encryption utility",
+	})
+	os.Exit(app.Run(config.TemporalConfig{}, nil, os.Args[1:]))
+}
