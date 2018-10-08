b12095f9d32129596d8648e13efb3b537fea1b2c
Robert Lin <robertlin1@gmail.com>
2018-10-08 02:24:44 -0700

    test: use constructor

--- encrypt_test.go
+++ encrypt_test.go
@@ -35,7 +35,7 @@ func Test_EncryptManager(t *testing.T) {
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
-			e := &EncryptManager{passphrase: []byte(tt.fields.passphrase)}
+			e := NewEncryptManager(tt.fields.passphrase)
 
 			// encrypt
 			encrypted, err := e.Encrypt(tt.args.r)
