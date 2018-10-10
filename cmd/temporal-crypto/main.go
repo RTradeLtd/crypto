0a99cee565d984104ad0cf2aa691d6f5a3cd6c39
Robert Lin <robertlin1@gmail.com>
2018-10-09 20:56:33 -0700

    cli: expose encrypt functionality via CLI

--- cmd/temporal-crypto/main.go
+++ cmd/temporal-crypto/main.go
@@ -47,6 +47,41 @@ var commands = map[string]cmd.Cmd{
 			}
 		},
 	},
+	"encrypt": cmd.Cmd{
+		Blurb: "encrypt file using Temporal's encryption format",
+		Description: `Encrypts given files using passphrase set in TEMPORAL_PASSPHRASE. Encrypted 
+		files are saved in ./<filename>.encrypted. Multiple files can be provided as arguments.`,
+		Action: func(cfg config.TemporalConfig, args map[string]string) {
+			p := os.Getenv("TEMPORAL_PASSPHRASE")
+			if p == "" {
+				log.Fatal("no passphrase provided in TEMPORAL_PASSPHRASE")
+			}
+
+			decrypt := crypto.NewEncryptManager(p)
+			for i := 2; i < len(os.Args); i++ {
+				f, err := os.Open(os.Args[i])
+				if err != nil {
+					log.Fatal(err)
+				}
+				out, err := decrypt.Encrypt(f)
+				if err != nil {
+					log.Fatal(err)
+				}
+
+				dir, err := os.Getwd()
+				if err != nil {
+					log.Fatal(err)
+				}
+
+				if err = ioutil.WriteFile(
+					filepath.Join(dir, filepath.Base(os.Args[i]))+".encrypted",
+					out, 0644,
+				); err != nil {
+					log.Fatal(err)
+				}
+			}
+		},
+	},
 }
 
 func main() {
