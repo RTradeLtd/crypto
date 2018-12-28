cfe91beebdae484eea8075490a2a900fdb673f96
potsables <postables@rtradetechnologies.com>
2018-12-27 17:04:48 -0800

    encrypt: add more thorough gcm tests

--- encrypt_test.go
+++ encrypt_test.go
@@ -3,6 +3,7 @@ package crypto
 import (
 	"bytes"
 	"encoding/hex"
+	"fmt"
 	"io"
 	"io/ioutil"
 	"reflect"
@@ -10,18 +11,103 @@ import (
 )
 
 func Test_EncryptManager_AES256_GCM(t *testing.T) {
-	original := []byte("helloworld")
-	e := NewEncryptManager("passphrase")
-	encryptedData, nonce, passphrase, err := e.EncryptGCM(bytes.NewReader(original))
+	// open a sample file
+	original, err := ioutil.ReadFile("README.md")
 	if err != nil {
-		t.Fatal(err)
+		t.Errorf("setup failed: %s", err)
+		return
 	}
-	data, err := e.DecryptGCM(bytes.NewReader(encryptedData), hex.EncodeToString(passphrase), hex.EncodeToString(nonce))
-	if err != nil {
-		t.Fatal(err)
+
+	type fields struct {
+		passphrase string
 	}
-	if string(data) != "helloworld" {
-		t.Fatal("bad data reconstructed")
+	type args struct {
+		r io.Reader
+	}
+	tests := []struct {
+		name           string
+		fields         fields
+		args           args
+		want           []byte
+		wantEncryptErr bool
+		wantDecryptErr bool
+	}{
+		{"valid passphrase 1", fields{"helloworld"}, args{bytes.NewReader(original)}, original, false, false},
+		{"valid short passphrase 1", fields{"a"}, args{bytes.NewReader(original)}, original, false, false},
+		{"valid passphrase 2", fields{"helloworld"}, args{bytes.NewReader(original)}, original, false, false},
+		{"valid short passphrase 2", fields{"a"}, args{bytes.NewReader(original)}, original, false, false},
+		{"valid passphrase 3", fields{"helloworld"}, args{bytes.NewReader(original)}, original, false, false},
+		{"valid short passphrase 4", fields{"a"}, args{bytes.NewReader(original)}, original, false, false},
+		{"invalid reader", fields{"helloworld"}, args{nil}, nil, true, true},
+	}
+	// these are used to ensure that we don't detect
+	// reused nonces and ciphers across multiple runs
+	var (
+		nonces        [][]byte
+		ciphers       [][]byte
+		encodedNonce  string
+		encodedCipher string
+	)
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			e := NewEncryptManager(tt.fields.passphrase)
+
+			// encrypt
+			encrypted, nonce, cipher, err := e.EncryptGCM(tt.args.r)
+			if (err != nil) != tt.wantEncryptErr {
+				t.Errorf("EncryptManager.EncryptGCM() error = %v, wantErr %v", err, tt.wantEncryptErr)
+			}
+			if nonce != nil {
+				// encode nonce
+				encodedNonce = hex.EncodeToString(nonce)
+				nonces = append(nonces, nonce)
+			}
+			if cipher != nil {
+				// encode cipher
+				encodedCipher = hex.EncodeToString(cipher)
+				ciphers = append(ciphers, cipher)
+			}
+
+			// decrypt
+			decrypted, err := e.DecryptGCM(bytes.NewReader(encrypted), encodedCipher, encodedNonce)
+			if (err != nil) != tt.wantDecryptErr {
+				t.Errorf("EncryptManager.DecryptGCM() = %v, want %v", err, tt.wantDecryptErr)
+			}
+			// check decrypted output
+			if !reflect.DeepEqual(decrypted, tt.want) {
+				t.Errorf("EncryptManager.EncryptGCM() = %v, want %v", decrypted, tt.want)
+			}
+			if tt.name == "invalid reader" {
+				return
+			}
+			// test EncryptCipherAndNonce
+			encrypted, err = e.EncryptCipherAndNonce(cipher, nonce)
+			if (err != nil) != tt.wantEncryptErr {
+				t.Errorf("EncryptManager.EncryptCipherAndNonce() = %v, want %v", err, tt.wantEncryptErr)
+			}
+			// decrypt output which was encrypted using AES256-CFB
+			decrypted, err = e.DecryptCFB(bytes.NewReader(encrypted))
+			if (err != nil) != tt.wantDecryptErr {
+				t.Errorf("EncryptManager.DecryptCFB() = %v, want %v", err, tt.wantDecryptErr)
+			}
+			expectedFormattedDecryptedOutput := fmt.Sprintf("Nonce:\t%s\nCipherKey:\t%s", encodedNonce, encodedCipher)
+			if !reflect.DeepEqual(string(decrypted), expectedFormattedDecryptedOutput) {
+				t.Fatal("failed to properly construct GCM decryption file")
+				return
+			}
+		})
+	}
+	// parse through nonces to ensure we don't have duplicates
+	found := make(map[string]bool)
+	for i, v := range nonces {
+		if found[hex.EncodeToString(v)] {
+			t.Fatal("reused nonce discovered")
+		}
+		if found[hex.EncodeToString(ciphers[i])] {
+			t.Fatal("reused ciphers discovered")
+		}
+		found[hex.EncodeToString(v)] = true
+		found[hex.EncodeToString(ciphers[i])] = true
 	}
 }
 
